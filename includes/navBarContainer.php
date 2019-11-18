5e2c7244a362c5b932863c2ee84b6870539aae5b
him3r4 <39840396+him3r4@users.noreply.github.com>
2019-11-18 14:37:24 +0100

    spotify

---
+++ includes/navBarContainer.php
@@ -0,0 +1,35 @@
+<div id="navBarContainer">
+
+    <nav class="navBar">
+
+        <span class="logo" onclick="openPage('index.php')">
+            <img src="assets/images/icons/logo.png">
+        </span>
+
+        <div class="group">
+            <div class="navItem">
+                <a href="search.php" class="navItemLink">Search
+                    <img src="assets/images/icons/search.png" class="icon" alt="Search">
+                </a>
+            </div>
+        </div>
+
+        <div class="group">
+
+            <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+            </div>
+
+            <div class="navItem">
+                <a href="yourMusic.php" class="navItemLink">Your Music</a>
+            </div>
+
+            <div class="navItem">
+                <a href="profile.php" class="navItemLink">Nino Zebic</a>
+            </div>
+
+
+        </div>
+
+</div>
+    </nav>
